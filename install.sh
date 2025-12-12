5c53dc714a771e42d8934cc63b056659a63eeb35
WindraSe <pemaingamez123@gmail.com>
2025-12-12 15:09:46 +0700

    Update installation script for bot-store

--- install.sh
+++ install.sh
@@ -6,33 +6,33 @@ NC='\033[0m' # No Color
 
 echo -e "${GREEN}[+] Memulai Instalasi WINTUNELING STORE BOT...${NC}"
 
-# 1. Update System & Install Git/Curl
+# Update System & Install Git/Curl
 echo -e "${GREEN}[+] Mengupdate sistem...${NC}"
 sudo apt-get update
 sudo apt-get install -y git curl unzip
 
-# 2. Install Node.js (Versi 18 LTS)
+# Install Node.js (Versi 18 LTS)
 echo -e "${GREEN}[+] Menginstall Node.js...${NC}"
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo -e "${GREEN}[+] Mengunduh Script Bot...${NC}"
-rm -rf wintuneling-bot
+rm -rf bot-store
 git clone https://github.com/windrase/bot-store.git bot-store
 
 cd bot-store
 
-# 4. Install Dependencies
+# Install Dependencies
 echo -e "${GREEN}[+] Menginstall Module (npm install)...${NC}"
 npm install
 npm install -g pm2
 
-# 5. Jalankan Setup
+# Jalankan Setup
 echo -e "${GREEN}[+] Menjalankan Setup Wizard...${NC}"
 echo "------------------------------------------------"
 node install.js
 
-# 6. Start Bot dengan PM2 (Agar jalan 24 jam)
+# Start Bot dengan PM2 (Agar jalan 24 jam)
 echo -e "${GREEN}[+] Menjalankan Bot di Background...${NC}"
 pm2 start index.js --name "bot-store"
 pm2 save
